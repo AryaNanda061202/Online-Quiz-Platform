3994553dfdb22c17c622fda4d90db6e2e48886c8
Arya Nanda <104360991+AryaNanda061202@users.noreply.github.com>
2026-01-28 22:00:16 +0530

    Delete OnlineQuizBackend directory

--- OnlineQuizBackend/src/main/java/com/example/quizapp/repository/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.quizapp.repository;
-
-import com.example.quizapp.entity.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
