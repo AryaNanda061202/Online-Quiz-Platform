f1c4eed0563657fcf5cb3bbfee2cbe9ce9c34a6e
Arya Nanda <104360991+AryaNanda061202@users.noreply.github.com>
2026-01-28 22:48:46 +0530

    Delete quizapp-master BackEnd directory

--- quizapp-master BackEnd/src/main/java/com/example/quizapp/repository/StudentQuizRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.quizapp.repository;
-
-import com.example.quizapp.entity.StudentQuiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface StudentQuizRepository extends JpaRepository<StudentQuiz, Long> {
-    List<StudentQuiz> findByStudentEmail(String email);
-}
